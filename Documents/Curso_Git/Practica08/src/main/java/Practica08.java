8d2f9e208469d505d12e4749fde9ba9e03ba79d5
Jose Rosas <jose.rosas@edu.uaa.mx>
2020-07-29 21:48:38 -0500

    #1, Mi Primer Commit #2, Primeras lineas de código

---
+++ Documents/Curso_Git/Practica08/src/main/java/Practica08.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ing_jd
+ */
+public class Practica08 {
+
+    public static void main(String[] args) {
+        System.out.println("Práctica 08 de GIT");
+    }
+   
+        
+}
